da5f18b4707a21a2d7789e636f0683f8b5d110e0
hodaya55 <hodayah159@gmail.com>
2018-03-18 01:34:12 +0200

    done exercise 2

--- main.js
+++ main.js
@@ -6,7 +6,56 @@ var reservations = {
 var name = prompt('Please enter the name for your reservation');
 
 var claimReservation = function () {
-  // write your code here!
+
+ var exist=false;
+var array=Object.keys(reservations); // array of the keys (which are the names..)
+
+ //for (var n in array)
+ for(var n=0; n<array.length && !exist; n++)
+    if (array[n].toLowerCase() == name.toLowerCase())
+      { 
+         exist=true;
+
+         if (reservations[array[n]].claimed== true) // the reservation claimed
+            alert("Hi, "+name+" .Your reservation already claimed");
+  
+        else // the reservation unclaimed
+            alert("Welcome "+name);
+      }
+
+if (!exist) // if the reservation doesnt exists
+      {
+       console.log("Hi, "+ name+ " there is no reservation under your name");
+       reservations[name]= {claimed: true};
+       alert("we set new reservation for you!");
+      }
+
+  /*
+  if (reservations[name]) // if the reservation exists
+  {
+    if (reservations[name].claimed== true) // the reservation claimed
+           alert("Hi, "+name+" .Your reservation already claimed");
+         
+    else // the reservation unclaimed
+           alert("Welcome "+name);
+  }
+  else // if the reservation doesnt exists
+      { console.log("Hi, "+ name+ " there is no reservation under your name");
+       reservations[name]= {claimed: true};
+       alert("we set new reservation for you!");
+      }
+      */
 }
 
 claimReservation();
+
+
+//example check
+/*
+var user ={ "1": 33,
+"2": 44};
+var b= Object.keys(user);
+for (var u in b)
+ if (b[u] == "2")
+    console.log( '2 nimza');
+*/
